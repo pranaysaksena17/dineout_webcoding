4d3905d6fe27ab7d85de7b7aecbceae8a5a86428
pranaysaksena17 <pranaysaksena17@users.noreply.github.com>
2021-09-09 13:39:54 +0530

    added practice components

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import Container from '@material-ui/core/Container';
 import DatePickers from '../Components/DateTimePicker';
+import ContainedButtons from '../Components/Button1';
+import MaterialUIPickers from '../Components/D&TPicker1';
+import FloatingActionButtons from '../Components/Float1';
+import SimpleBottomNavigation from '../Components/BottomNav1';
+import BasicButtonGroup from '../Components/BasicButtonGroup';
+import DelayingAppearance from '../Components/DelayApp';
+import DetailedAccordion from '../Components/Accordian1';
 
 export default function Home() {
   return (
@@ -13,6 +20,13 @@ export default function Home() {
                 To get started, edit <code>src/App.js</code>, save, and reload this page.
             </p>
             <DatePickers/>
+            <ContainedButtons/>
+            <FloatingActionButtons/>
+            <BasicButtonGroup/>
+            <MaterialUIPickers/>
+            <DetailedAccordion/>
+            <DelayingAppearance/>
+            <SimpleBottomNavigation/>
         </Container>
     </React.Fragment>
   );
--- src/Pages/Hotels.js
+++ src/Pages/Hotels.js
@@ -2,6 +2,12 @@ import React from 'react';
 import Container from '@material-ui/core/Container';
 import DatePickers from '../Components/DateTimePicker';
 import SimpleCard from '../Components/HomeMultiCard';
+import Checkboxes from '../Components/BasicCheckboxes1';
+import CustomizedSnackbars from '../Components/CustomSnack1';
+import IconBreadcrumbs from '../Components/IconBreadcrumbs';
+import ImageAvatars from '../Components/Avatars';
+import TransferList from '../Components/TransferList1';
+import SimplePortal from '../Components/SimplePortal1';
 
 export default function Hotels() {
   return (
@@ -13,8 +19,14 @@ export default function Hotels() {
             <p>
                 To get started, edit <code>src/App.js</code>, save, and reload this page.
             </p>
+            <IconBreadcrumbs/>
+            <ImageAvatars/>
             <DatePickers/>
             <SimpleCard/>
+            <Checkboxes/>
+            <TransferList/>
+            <SimplePortal/>
+            <CustomizedSnackbars/>
         </Container>
     </React.Fragment>
   );
